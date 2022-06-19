8bb0005056fa99ba3817d9e2c524e1b70878f9db
Sitikantha Muhuri <sitikanthamuhuri@gmail.com>
2022-06-19 20:34:21 +0530

    DAS

---
+++ header.php
@@ -0,0 +1,133 @@
+ <!-- Header Start -->
+ <header class="header-style-fullwidth">
+        <div class="top-bar-right d-flex align-items-center text-md-left">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col d-flex align-items-center contact-info">
+                        <div>
+                            <i class="fas fa-phone-alt"></i> <a href="tel:">(+91) 33-2441-5124 </a>
+                        </div>
+
+                        <div>
+                            <i class="fas fa-phone-alt"></i> <a href="tel:">(+91) 33-2441-4431 </a>
+                        </div>
+
+                        <div>
+                            <i class="far fa-envelope"></i> <a href="#">developmentactionsociety@gmail.com</a>
+                        </div>
+
+                        <!------
+                        <div>
+                            <i class="far fa-clock"></i>  Mon-Fri  /  10:00 AM - 6:00 PM
+                        </div>
+
+                        ----->
+                    </div>
+
+                    <div class="col-md-auto">
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com/daskolkata1989/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        
+                            <a href="https://instagram.com/developmentactionsociety_das?utm_medium=copy_link" target="_blank"><i class="icofont-instagram"></i></a>
+
+                            <!----
+                            <a href="#"><i class="icofont-behance"></i></a>
+
+                            ---->
+                            <a href="https://www.youtube.com/channel/UCBiIADqIm8D1zGdBizj0wjg/featured" target="_blank"><i class="icofont-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Main Navigation Start -->
+        <nav class="navbar navbar-expand-lg header-fullpage">
+            <div class="container text-nowrap">
+                <div class="d-flex align-items-center w-100 col p-0 logo-brand">
+                    <a class="navbar-brand rounded-bottom light-bg" href="index.html">
+                        <img src="assets/images/logo.png" alt="logo" class="img-fluid">
+                    </a> 
+                </div>
+                <!-- Topbar Buttons Start -->
+                <div class="d-inline-flex request-btn order-lg-last col-auto p-0 align-items-center"> 
+                    <a class="btn-outline-primary btn ml-3" href="#" id="search_home"><i data-feather="search"></i></a>
+
+                    <a class="nav-link btn btn-default ml-3 donate-btn" href="donation-page.html">Donate</a>
+
+                    <!-- Toggle Button Start -->
+                    <button class="navbar-toggler x collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Toggle Button End -->  
+                </div>
+                <!-- Topbar Buttons End -->
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " href="index.html">Home</a>                
+                           
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about-us.html">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle-mob" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programs <i class="icofont-rounded-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="our-programs.html">Our Programs</a></li>                    
+                                <li><a class="dropdown-item" href="education.html">Education</a></li>
+                                <li><a class="dropdown-item" href="child-protect.html">Child Protection</a></li> 
+                                <li><a class="dropdown-item" href="public-health.html">Public Health</a></li>
+                                <li><a class="dropdown-item" href="WASH.html">Water Sanitation Hygiene(WASH)</a></li>  
+                                <li><a class="dropdown-item" href="livelihood.html">Livelihood</a></li>
+                                <li><a class="dropdown-item" href="women-emp.html">Women Empowerment</a></li>    
+
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="our-blog-grid.html">Impact</a>
+                        
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="our-partners.html">Our Partners</a>
+                         
+                        </li>                        
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle-mob" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Get Involved <i class="icofont-rounded-down"></i></a>
+
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="get-involved.html">How to get involved</a></li>
+                                <li><a class="dropdown-item" href="donation-page.html">Donate</a></li>
+                                <li><a class="dropdown-item" href="become-volunteers.html">Volunteer</a></li>
+                                <li><a class="dropdown-item" href="jobs.html">Jobs</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="media.html">Media</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.html">Gallery</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="archive-or-pub.html">Archive/Publication</a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact-us.html">Contact</a>
+                        </li>
+                        
+                    </ul>
+                    <!-- Main Navigation End -->
+                </div>
+            </div>
+        </nav>
+        <!-- Main Navigation End -->
+    </header>
+    <!-- Header Start -->
